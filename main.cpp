c6b326059cf350c6e725bd46a3b17b3beac99a69
ajp109 <andrew.pomfret@york.ac.uk>
2022-08-02 13:50:23 +0000

    Correct code in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include "mbed.h"
 
-int main() {
-    while (true) {
-        sleep();
+int main()
+{
+    int var = 10;
+    
+    printf("var: %d\n", var);
+    if (var > 6) {
+        printf("var is greater than 6\n");
     }
+    
+    var = 3;
+    
+    printf("var: %d\n", var);
+    if (var < 5) {
+        printf("var is less than 5\n");
+    }
+
+
+    // Do nothing, forever...
+    while (true);
 }
